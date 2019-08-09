c50604b6e83e3f61bd5551285c9fbfa2a7eb6c49
idealase <brodie.sandford@gmail.com>
2019-08-09 14:37:58 +0800

    add bubble_sort

--- Prac1/SortFilesPython/DSAsorts.py
+++ Prac1/SortFilesPython/DSAsorts.py
@@ -5,33 +5,33 @@
 #
 
 my_list = [29, 5, 18, 13, 12, 20, 50, 214, 1]
+verbose_output = True
 
 
 def bubble_sort(input_array):
-    passno = 0
-    sorted = False
+    pass_num = 0
+    is_sorted = False
 
-    while not sorted:
-        sorted = True   # assume sorted - we'll find out if its not
-        for i in range(0, (len(input_array) - 1 - passno)):
+    while not is_sorted:
+        is_sorted = True   # assume sorted - we'll find out if its not
+        for i in range(0, (len(input_array) - 1 - pass_num)):
             if input_array[i] > input_array[i + 1]:
-                previousA = input_array   # FIXME: not assigning correctly
+                prev_array = input_array.copy()
                 temp = input_array[i]     # swap elements i and i+1
                 input_array[i] = input_array[i + 1]
                 input_array[i + 1] = temp
-                sorted = False  # still need to continue sorting
-                print("On Pass " + str(passno+1) + ": Swapped " + str(input_array[i]) +
-                      " with " + str(input_array[i + 1]))
-                print(str(previousA) + " became " + str(input_array) + "\n")
-        passno += 1
+                is_sorted = False  # still need to continue sorting
+                if verbose_output:
+                    print("On Pass " + str(pass_num+1) + ": Swapped " +
+                          str(input_array[i+1]) + " with " +
+                          str(input_array[i]))
+                    print(str(prev_array) + " -----> " +
+                          str(input_array) + "\n")
+        pass_num += 1
+    print("Sorted list: " + str(input_array))
         
 
 bubble_sort(my_list)
-
-
-
-
-
 
 
 
