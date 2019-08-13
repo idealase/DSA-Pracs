5a630ba57cddd1aa760bc2ec5838cdc486981f80
idealase <brodie.sandford@gmail.com>
2019-08-13 15:46:34 +0800

    init hanoi

---
+++ Prac2/Hanoi.py
@@ -0,0 +1,20 @@
+"""
+Implement Towers of Hanoi algorithm as a python method
+"""
+
+
+def move_disk(src, dest):
+    print("moving top disk from peg " + str(src) + " to peg " + str(dest))
+
+
+# move n disks from peg src to peg dest
+def towers(n, src, dest):
+    if n == 1:
+        move_disk = (src, dest)
+    else:
+        temp = 6 - src - dest    # the other (non-target) peg
+        towers = (n-1, src, temp)
+        move_disk = (src, dest)
+        towers = (n-1, temp, dest)
+
+towers(3, 1, 2)
