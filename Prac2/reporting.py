ae201228d0c70189396f8f8f8a721b10f1a757ec
idealase <brodie.sandford@gmail.com>
2019-08-14 09:42:34 +0800

    init reporting

---
+++ Prac2/reporting.py
@@ -0,0 +1,15 @@
+import platform, time
+
+
+def gen_report():
+    # time report
+    time_now = time.localtime()
+    print("Date: " + str(time_now[2]) + "-" + str(time_now[1]) + "-" +
+          str(time_now[0]))
+    print("Time: " + str(time_now[3])+ ":" + str(time_now[4]) + ":" +
+          str(time_now[5]))
+
+    # system information
+    print("Python " + str(platform.python_version()))
+    print(platform.machine(), platform.platform(), platform.processor())
+
