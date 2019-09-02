f767635b8ae867d5cefbd0110516c7c9aad8e065
idealase <brodie.sandford@gmail.com>
2019-09-03 05:21:58 +0800

    init double ended ll

---
+++ Prac3/de_linked.py
@@ -0,0 +1,88 @@
+class DSAListNode:
+    def __init__(self, in_value):
+        self.value = in_value
+        self.next = None
+
+    def get_value(self):
+        """this will get the value"""
+        return self.value
+
+    def set_value(self, in_value):
+        self.value = in_value
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+
+class DSALinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def is_empty(self):
+        empty = (self.head is None)
+        return empty
+
+    def insert_first(self, new_value):
+        new_node = DSAListNode(new_value)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
+
+    def insert_last(self, new_value):
+        new_node = DSAListNode(new_value)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:   # TODO: change to double ended method...
+            curr_node = self.head
+            while curr_node.get_next():
+                curr_node = curr_node.get_next()
+            curr_node.set_next(new_node)
+
+    def peek_first(self):
+        if self.is_empty():
+            raise ValueError
+        else:
+            node_value = self.head.get_value()
+            return node_value
+
+    def peek_last(self):
+        if self.is_empty():
+            raise ValueError
+        else:
+            curr_node = self.head
+            while curr_node.get_next():
+                curr_node = curr_node.get_next()
+            node_value = curr_node.get_value()
+            return node_value
+
+    def remove_first(self):     # TODO: clarify how tail affects this
+        if self.is_empty():
+            raise ValueError
+        else:
+            node_value = self.head.get_value()
+            self.head = self.head.get_next()
+            return node_value
+
+    def remove_last(self):
+        if self.is_empty():
+            raise ValueError
+        elif not self.head.get_next():
+            node_value = self.head.get_value()
+            self.head = None
+        else:
+            prev_node = None
+            curr_node = self.head
+            while curr_node.get_next():
+                prev_node = curr_node
+                curr_node = curr_node.get_next()
+            prev_node.set_next(None)
+            node_value = curr_node.get_value()
+        return node_value
