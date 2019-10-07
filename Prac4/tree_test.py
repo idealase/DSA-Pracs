e051b26d80cf31cdb7cc3d1714d73505540705a5
idealase <brodie.sandford@gmail.com>
2019-10-07 15:16:56 +0800

    expand iterative display

--- Prac4/tree_test.py
+++ Prac4/tree_test.py
@@ -14,22 +14,39 @@ keys = [2,3,4,5,6,8,9,10,12,15,16,166,424,24,42,63,57]
 my_node = tree.DSATreeNode(1, "matt")
 print("Displaying node: ")
 print(my_node)
-print("\n")
 
 
 my_tree = tree.DSABinarySearchTree()
 my_tree.insert(7, random.choice(names))
 
+count = 1
 for i in range(0, 10):
     temp_key = random.choice(keys)
     keys.remove(temp_key)
+
     temp_name = random.choice(names)
     names.remove(temp_name)
+
     my_tree.insert(temp_key, temp_name)
 
+    print("\nOn iteration " + str(count) + ", added key " + str(temp_key) +
+          " with value " + str(temp_name))
+    max_node = my_tree.max()
+    print("Max: " + str(max_node))
+    min_node = my_tree.min()
+    print("Min: " + str(min_node))
+    count += 1
 
+
+print("\nFinal tests...")
 found = my_tree.find(7)
 print(found)
+
+max_node = my_tree.max()
+print(max_node)
+
+min_node = my_tree.min()
+print(min_node)
 
 
 
