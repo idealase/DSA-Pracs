9e47acaf2f1f0f25135af4c17e85c8fa47dae16f
idealase <brodie.sandford@gmail.com>
2019-10-08 11:02:26 +0800

    update VE string

--- Prac4/treenode.py
+++ Prac4/treenode.py
@@ -71,7 +71,7 @@ class DSABinarySearchTree:
         if not curr:  # base case - found
             update_node = DSATreeNode(key, value)
         elif key == curr.get_key():
-            raise ValueError("Key {} Already in tree".format(key))
+            raise ValueError("Key {0} Already in tree".format(key))
         elif key <= curr.get_key():
             curr._left = self._insert_rec(key, value, curr.get_left())
         else:
