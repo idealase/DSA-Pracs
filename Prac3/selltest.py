9d91256fd8ff2ab5439a8a30b59ca5351e0091ac
idealase <brodie.sandford@gmail.com>
2019-10-15 18:48:49 +0800

    test formatting

--- Prac3/selltest.py
+++ Prac3/selltest.py
@@ -1,40 +1,45 @@
 import se_linked
+from random import randint
 
 my_list = se_linked.DSALinkedList()
 
+print("Empty Status: " + str(my_list.is_empty()))
 
 my_list.insert_first(29)
 my_list.insert_last(40)
 
 print("Empty Status: " + str(my_list.is_empty()))
-print(my_list.peek_first())
-print(my_list.peek_last())
+print("First: {0}\t Last: {1}".format
+      (my_list.peek_first(), my_list.peek_last()))
 
 print("\n Inserting at first and last positions...")
 my_list.insert_last(42)
 my_list.insert_first(28)
-print(my_list.peek_first(), my_list.peek_last())
+print("First: {0}\t Last: {1}".format
+      (my_list.peek_first(), my_list.peek_last()))
 
 
 print("\n Removing first and last...")
 my_list.remove_first()
 my_list.remove_last()
-print(my_list.peek_first(), my_list.peek_last())
+print("First: {0}\t Last: {1}".format
+      (my_list.peek_first(), my_list.peek_last()))
 
 
 
 # testing the iterator
 ll = se_linked.DSALinkedList()
-ll.insert_first(4)
-ll.insert_first(44)
-ll.insert_first(53)
-ll.insert_first(29)
-ll.insert_first(7)
+rand_range = randint(10, 25)
+for i in range(15, rand_range):
+    new_node = randint(0, 100)
+    ll.insert_first(new_node)
+
 
 # creating list and getting next value
 myiter = iter(ll)
 value = next(myiter)
 
+print("\nTraversing list...", end="\t\t")
 # using a for loop to go through list
 for value in ll:
-    print(value, end="-->")
+    print(value, end=" --> ")
