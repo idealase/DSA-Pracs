147c3ae8cc56c5f3fb91729fad43952349339fc8
idealase <brodie.sandford@gmail.com>
2019-10-22 04:04:16 +0800

    init socialsim

---
+++ SocialSim/SocialSim.py
@@ -0,0 +1,30 @@
+""""
+SocialSim.py
+
+"""
+import sys
+
+usage = "\t---\tWelcome to SocialSim.py\t---\t\n " \
+        "\n\n...\n\n\n" \
+        "Instructions...\n\n" \
+        "select \'-i\' for ..." \
+        "\n\t---\tInteractive Test Mode\t---\t\n " \
+        "select \'-s\' for ..." \
+        "\n\t---\tSimulation Mode\t---\t\n " \
+        "\n\ne.g.\n\t" \
+        ">> python SocialSim.py -s netfile eventfile p_like p_follow" \
+
+
+try:
+    run_mode = sys.argv[1]
+except IndexError:
+    print(usage)
+    exit()
+
+
+if run_mode == "-i":
+    print("Int")
+elif run_mode == "-s":
+    print("Sim")
+else:
+    print("Hmmmmmm")
